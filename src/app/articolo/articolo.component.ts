ca975875214d0dee8eac86eaebd4d5c8ffb9d0f7
crypt0r3bel <crypt0r3bel@gmail.com>
2018-01-23 15:03:59 +0100

    implementazione articolo

--- src/app/articolo/articolo.component.ts
+++ src/app/articolo/articolo.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-//test
+import { Component } from '@angular/core';
+import { Articolo }  from './articolo'
+  
 @Component({
-  selector: 'app-articolo',
-  templateUrl: './articolo.component.html',
-  styleUrls: ['./articolo.component.css']
+    selector: 'articolo',
+    templateUrl: 'articolo.component.html',
+    styleUrls: ['articolo.component.css']
 })
-export class ArticoloComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ArticoloComponent {
+    articolo: Articolo;
+  
+    constructor() {
+        this.articolo = {
+            titolo: "Creare componenti Angular 2",
+            autore: "Mario Rossi",
+            testo:  "Creare componenti con Angular 2 è molto semplice."
+        };
+    }
 }
